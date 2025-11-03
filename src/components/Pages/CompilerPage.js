374b4c98029ca112b2a223506ab647ca154b4f84
Jeremiah Sheehy <jameys127@gmail.com>
2025-11-03 00:15:24 -0800

    fixed an issue where css was infront of page messing up buttons

--- src/components/Pages/CompilerPage.js
+++ src/components/Pages/CompilerPage.js
@@ -4,6 +4,7 @@ import { vscodeDark } from '@uiw/codemirror-theme-vscode';
 import './CompilerPage.css';
 import { ReactTerminal } from 'react-terminal';
 import Terminal from '../addons/Terminal';
+import { Link } from 'react-router-dom';
 
 const CompilerPage = () => {
   const [program, setProgram] = useState('');
@@ -21,10 +22,10 @@ const CompilerPage = () => {
       <CodeMirror className='w-1/2 p-3' value={code} height='80vh' theme={vscodeDark} onChange={onChange}/>
       <div className='flex flex-col p-3 w-1/2 text-[#fff]'>
         <div className='flex flex-col h-[40vh] p-2 justify-center items-center'>
-          <h1>JavaEsque Compiler</h1>
-          <p>Stuff</p>
+          <h1 className='text-blue-200 font-bold text-[2.3rem] font-mono'>JavaEsque Compiler</h1>
+          <p className='text-center'>My custom language built in Java that compiles to Javascript.<br/>You can find the source code on <a href='https://github.com/jameys127'>Github</a></p>
         </div>
-        <div className='flex flex-col h-[40vh] max- '>
+        <div className='flex flex-col h-[40vh]'>
           <Terminal output={output} />
         </div>
       </div>
--- src/components/addons/Terminal.js
+++ src/components/addons/Terminal.js
@@ -1,4 +1,5 @@
 import React, {useEffect, useRef} from 'react'
+import './Terminal.css'
 
 const Terminal = ({output}) => {
   const containerRef = useRef();
@@ -9,13 +10,13 @@ const Terminal = ({output}) => {
     const formatted = output
         .split('\n')
         .map(line => `> ${line}`)
-        .join('\n');
-    el.innerText = formatted;
+        .join('<br>');
+    el.innerHTML = `${formatted}<span class="cursor">█</span>`;
     el.scrollTop = el.scrollHeight;
   }, [output]);
 
   return (
-    <div className='bg-[#111] min-h-full max-h-full p-2 text-green-500 font-mono rounded-md border border-[#333333] border-solid overflow-y-scroll' ref={containerRef} />
+    <div className='terminal bg-[#111] min-h-full max-h-full p-2 text-[#e76fff] font-mono rounded-md border border-[#333333] border-solid overflow-y-scroll z-0' ref={containerRef} />
   );
 };
 
